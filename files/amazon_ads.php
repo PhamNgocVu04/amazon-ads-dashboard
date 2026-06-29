e9a4a9dd0c1c56849a398924f99753c5071233b0
phamngocvu04 <phamngocvu051@gmail.com>
2026-06-29 10:47:22 +0700

    chore: remove temp files

--- files/amazon_ads.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'account_id' => (int) env('ACCOUNT_ID', 0),
-    'mkp_id' => (int) env('MKP_ID', 1),
-];
--- files/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\DashboardController;
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('amazon-ads')->group(function () {
-    Route::get('/dashboard/campaigns', [DashboardController::class, 'campaigns']);
-    Route::get('/dashboard/daily', [DashboardController::class, 'daily']);
-});
